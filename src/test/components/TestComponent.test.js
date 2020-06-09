09249c9b904b07a61e23ffd407718c9658417c39
j0nesma <matthewarjones@outlook.com>
2020-06-09 22:24:36 +0100

    Adding example jest test file

--- src/test/components/TestComponent.test.js
+++ src/test/components/TestComponent.test.js
@@ -1,10 +1,12 @@
 import { mount } from '@vue/test-utils'
-
+import '@testing-library/jest-dom'
 import Test from 'components/Test.vue'
 
 describe('Component', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Test)
-    expect(wrapper.isVueInstance()).toBeTruthy()
+    const wrapper = mount(Test);
+    expect(wrapper.get('span').element).toBeTruthy();
+    expect(wrapper.find('span').element).toBeVisible();
+    expect(wrapper.find('span').element.innerHTML).toBe('Testing');
   })
 })
