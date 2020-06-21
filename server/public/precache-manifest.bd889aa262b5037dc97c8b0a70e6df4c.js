f1b75e98d4f8c638a87442eab84fad77faa0b8d1
j0nesma <matthewarjones@outlook.com>
2020-06-21 22:48:45 +0100

    adding public

---
+++ server/public/precache-manifest.bd889aa262b5037dc97c8b0a70e6df4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f37f9d4d44d361f0ded",
+    "url": "/css/app.fe46a6c8.css"
+  },
+  {
+    "revision": "1a119a293c6dfe6ec7a5",
+    "url": "/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "70e9c52ac0c6a63cf47ddcb2e14a166b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f37f9d4d44d361f0ded",
+    "url": "/js/app.f4004634.js"
+  },
+  {
+    "revision": "1a119a293c6dfe6ec7a5",
+    "url": "/js/chunk-vendors.aecdab62.js"
+  },
+  {
+    "revision": "51286b887ad9ab1e55e47c14c44b30d8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
